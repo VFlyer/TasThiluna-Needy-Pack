9b292b772bb49d7d7d19b8d2d5032bedc1ec4e23
Brendan King <brendan@brendanking.com>
2020-04-11 03:04:52 -0400

    Added TP support, tp autosolvers, and general updates to all modules
    General Updates:
    - Made it so no buttons or highlights are floating
    - Added Gamepad/controller support to all mods
    - Improved some modules text objects

--- Assets/hyperneedy.cs
+++ Assets/hyperneedy.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using UnityEngine;
 using KModkit;
 using rnd = UnityEngine.Random;
+using System.Text.RegularExpressions;
 
 public class hyperneedy : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class hyperneedy : MonoBehaviour
     private Color[] usedColors = new Color[16];
 
     private bool active;
+    private bool bombSolved;
     private bool animating;
     private bool discsOut;
     private int rotationIndex;
@@ -37,6 +39,8 @@ public class hyperneedy : MonoBehaviour
         module.OnNeedyActivation += OnNeedyActivation;
         module.OnNeedyDeactivation += OnNeedyDeactivation;
         module.OnTimerExpired += OnTimerExpired;
+        bomb.OnBombExploded += delegate () { bombSolved = true; };
+        bomb.OnBombSolved += delegate () { bombSolved = true; };
         button.OnInteract += delegate () { ButtonPress(); return false; };
         foreach (KMSelectable axisButton in axisButtons)
             axisButton.OnInteract += delegate () { AxisButtonPress(axisButton); return false; };
@@ -58,6 +62,7 @@ public class hyperneedy : MonoBehaviour
     {
         active = true;
         rotationIndex = rnd.Range(0, 12);
+        Debug.LogFormat("[Hyperneedy #{0}] The rotation is {1}.", moduleId, rotationNames[rotationIndex]);
         var vertices = GetUnrotatedVertices();
         for (int i = 0; i < 16; i++)
             StartCoroutine(Move(allDiscs[i], new Vector3(0f, 0f, 0f), vertices[i].Project(), true));
@@ -96,16 +101,19 @@ public class hyperneedy : MonoBehaviour
         axisButton.AddInteractionPunch(.5f);
         if (axisButton.GetComponentInChildren<TextMesh>().text != currentRotation[enteringStage].ToString())
         {
+            Debug.LogFormat("[Hyperneedy #{0}] Pressed the button {1}, that is incorrect. Strike!", moduleId, axisButton.GetComponentInChildren<TextMesh>().text);
             module.OnStrike();
             enteringStage = 0;
         }
         else
         {
+            Debug.LogFormat("[Hyperneedy #{0}] Pressed the button {1}, that is correct.", moduleId, axisButton.GetComponentInChildren<TextMesh>().text);
             enteringStage++;
             audio.PlaySoundAtTransform("Bleep" + rnd.Range(1, 11).ToString(), axisButton.transform);
         }
         if (enteringStage == 2)
         {
+            Debug.LogFormat("[Hyperneedy #{0}] Pressed the button {1}, that is correct. Module temporarily neutralized!", moduleId, axisButton.GetComponentInChildren<TextMesh>().text);
             module.OnPass();
             OnNeedyDeactivation();
         }
@@ -179,4 +187,106 @@ public class hyperneedy : MonoBehaviour
         return Enumerable.Range(0, 1 << 4).Select(i => new Point4D((i & 1) != 0 ? 1 : -1, (i & 2) != 0 ? 1 : -1, (i & 4) != 0 ? 1 : -1, (i & 8) != 0 ? 1 : -1)).ToArray();
     }
 
+    // Twitch Plays
+    #pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"!{0} rotate (#) [Starts the rotation of the floating discs (Optionally '#' times)] | !{0} press <rot> [Presses the buttons corresponding to the rotation 'rot'] | Valid rotations are XY, YX, XZ, ZX, XW, WX, YZ, ZY, YW, WY, ZW, and WZ";
+    #pragma warning restore 414
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        string[] parameters = command.Split(' ');
+        if (Regex.IsMatch(parameters[0], @"^\s*rotate\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            yield return null;
+            if (parameters.Length > 2)
+            {
+                yield return "sendtochaterror Too many parameters!";
+            }
+            else if (parameters.Length == 2)
+            {
+                int temp = 0;
+                if (int.TryParse(parameters[1], out temp))
+                {
+                    if (temp < 1 || temp > 5)
+                    {
+                        yield return "sendtochaterror The specified number of times to rotate '" + parameters[1] + "' is out of range 1-5!";
+                        yield break;
+                    }
+                    for (int i = 0; i < temp; i++)
+                    {
+                        button.OnInteract();
+                        while (animating) { yield return "trycancel Halted rotating due to a request to cancel!"; yield return new WaitForSeconds(0.1f); }
+                    }
+                }
+                else
+                {
+                    yield return "sendtochaterror The specified number of times to rotate '" + parameters[1] + "' is invalid!";
+                }
+            }
+            else if (parameters.Length == 1)
+            {
+                button.OnInteract();
+                while (animating) { yield return new WaitForSeconds(0.1f); }
+            }
+            yield break;
+        }
+        if (Regex.IsMatch(parameters[0], @"^\s*press\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            yield return null;
+            if (parameters.Length > 2)
+            {
+                yield return "sendtochaterror Too many parameters!";
+            }
+            else if (parameters.Length == 2)
+            {
+                parameters[1] = parameters[1].ToUpper();
+                if (rotationNames.Contains(parameters[1]))
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (axisButtons[i].GetComponentInChildren<TextMesh>().text[0].Equals(parameters[1][0]))
+                        {
+                            axisButtons[i].OnInteract();
+                            yield return new WaitForSeconds(0.1f);
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (axisButtons[i].GetComponentInChildren<TextMesh>().text[0].Equals(parameters[1][1]))
+                        {
+                            axisButtons[i].OnInteract();
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    yield return "sendtochaterror The specified rotation '"+parameters[1]+"' is invalid!";
+                }
+            }
+            else if (parameters.Length == 1)
+            {
+                yield return "sendtochaterror Please specify the rotation!";
+            }
+            yield break;
+        }
+    }
+
+    void TwitchHandleForcedSolve()
+    {
+        //The code is done in a coroutine instead of here so that if the solvebomb command was executed this will just input the number right when it activates and it wont wait for its turn in the queue
+        StartCoroutine(HandleSolve());
+    }
+
+    IEnumerator HandleSolve()
+    {
+        while (!bombSolved)
+        {
+            while (!active) { yield return new WaitForSeconds(0.1f); }
+            while (animating && !discsOut) { yield return new WaitForSeconds(0.1f); }
+            if (enteringStage == 1)
+                enteringStage = 0;
+            yield return ProcessTwitchCommand("press "+rotationNames[rotationIndex]);
+        }
+    }
 }
--- Assets/marcoPolo.cs
+++ Assets/marcoPolo.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using UnityEngine;
 using KModkit;
 using rnd = UnityEngine.Random;
+using System.Text.RegularExpressions;
 
 public class marcoPolo : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class marcoPolo : MonoBehaviour
     public Transform[] positions;
 
     private bool active;
+    private bool bombSolved;
     private bool isBlue;
     private bool swapped;
     private int directionIndex;
@@ -35,6 +37,8 @@ public class marcoPolo : MonoBehaviour
         module.OnNeedyActivation += OnNeedyActivation;
         module.OnNeedyDeactivation += OnNeedyDeactivation;
         module.OnTimerExpired += OnTimerExpired;
+        bomb.OnBombExploded += delegate () { bombSolved = true; };
+        bomb.OnBombSolved += delegate () { bombSolved = true; };
         soundButton.OnInteract += delegate () { PressSoundButton(); return false; };
         foreach (KMSelectable button in buttons)
             button.OnInteract += delegate () { PressButton(button); return false; };
@@ -86,6 +90,10 @@ public class marcoPolo : MonoBehaviour
             t.color = !isBlue ? textColors[0] : textColors[1];
             t.text = !swapped ? labels[Array.IndexOf(buttonTexts, t)] : labels[Enumerable.Range(0, 2).Where(x => x != Array.IndexOf(buttonTexts, t)).First()];
         }
+        Debug.LogFormat("[Marco Polo #{0}] The sound is coming from the {1}.", moduleId, directionIndex == 0 ? "left" : "right");
+        Debug.LogFormat("[Marco Polo #{0}] The left button says {1} and the right button says {2}.", moduleId, buttonTexts[0].text, buttonTexts[1].text);
+        Debug.LogFormat("[Marco Polo #{0}] The buttons are colored {1}.", moduleId, isBlue ? "blue" : "black");
+        Debug.LogFormat("[Marco Polo #{0}] The correct button to press is the {1} button.", moduleId, solution == 0 ? "left" : "right");
     }
 
     protected void OnNeedyDeactivation()
@@ -119,11 +127,99 @@ public class marcoPolo : MonoBehaviour
         button.AddInteractionPunch(.5f);
         audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, button.transform);
         if (Array.IndexOf(buttons, button) != solution)
+        {
+            Debug.LogFormat("[Marco Polo #{0}] Pressed the {1} button, which was incorrect. Strike!", moduleId, Array.IndexOf(buttons, button) == 0 ? "left" : "right");
             module.OnStrike();
+        }
         else
         {
+            Debug.LogFormat("[Marco Polo #{0}] Pressed the {1} button, which was correct. Module temporarily neutralized!", moduleId, Array.IndexOf(buttons, button) == 0 ? "left" : "right");
             OnNeedyDeactivation();
             module.OnPass();
+        }
+    }
+
+    // Twitch Plays
+    #pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"!{0} press <pos> (#) [Presses button in position 'pos' (Optionally '#' times if it is the center button)] | Valid positions are left, right, and center";
+    #pragma warning restore 414
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        string[] parameters = command.Split(' ');
+        if (Regex.IsMatch(parameters[0], @"^\s*press\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            yield return null;
+            if (parameters.Length > 3)
+            {
+                yield return "sendtochaterror Too many parameters!";
+            }
+            else if (parameters.Length == 3)
+            {
+                if (parameters[1].EqualsIgnoreCase("center"))
+                {
+                    int temp = 0;
+                    if (int.TryParse(parameters[2], out temp))
+                    {
+                        if (temp < 1 || temp > 10)
+                        {
+                            yield return "sendtochaterror The specified number of times to press the center button '" + parameters[2] + "' is out of range 1-10!";
+                            yield break;
+                        }
+                        for (int i = 0; i < temp; i++)
+                        {
+                            soundButton.OnInteract();
+                            yield return new WaitForSeconds(0.5f);
+                        }
+                    }
+                    else
+                    {
+                        yield return "sendtochaterror The specified number of times to press the center button '" + parameters[2] + "' is invalid!";
+                    }
+                }
+                else if (parameters[1].EqualsIgnoreCase("left") || parameters[1].EqualsIgnoreCase("right"))
+                {
+                    yield return "sendtochaterror Cannot press the '" + parameters[1] + "' button a certain number of times!";
+                }
+                else
+                {
+                    yield return "sendtochaterror Cannot press the '" + parameters[1] + "' button as it is invalid!";
+                }
+            }
+            else if (parameters.Length == 2)
+            {
+                if (parameters[1].EqualsIgnoreCase("center"))
+                {
+                    soundButton.OnInteract();
+                }
+                else if (parameters[1].EqualsIgnoreCase("left"))
+                {
+                    buttons[0].OnInteract();
+                }
+                else if (parameters[1].EqualsIgnoreCase("right"))
+                {
+                    buttons[1].OnInteract();
+                }
+                else
+                {
+                    yield return "sendtochaterror Cannot press the '" + parameters[1] + "' button as it is invalid!";
+                }
+            }
+            yield break;
+        }
+    }
+
+    void TwitchHandleForcedSolve()
+    {
+        //The code is done in a coroutine instead of here so that if the solvebomb command was executed this will just input the number right when it activates and it wont wait for its turn in the queue
+        StartCoroutine(HandleSolve());
+    }
+
+    IEnumerator HandleSolve()
+    {
+        while (!bombSolved)
+        {
+            while (!active) { yield return new WaitForSeconds(0.1f); }
+            buttons[solution].OnInteract();
         }
     }
 }
